f8295031a6abb96c90b732d0299a571ec054fe2d
mbgdatadev <108692949+mbgdatadev@users.noreply.github.com>
2023-12-08 00:49:58 +0300

    Create text-visualization.py

---
+++ Text-Visualization/text-visualization.py
@@ -0,0 +1,63 @@
+
+##################################################
+# 2. Text Visualization
+##################################################
+
+
+###############################
+# Terim Frekanslarının Hesaplanması
+###############################
+
+tf = df["reviewText"].apply(lambda x: pd.value_counts(x.split(" "))).sum(axis=0).reset_index()
+
+tf.columns = ["words", "tf"]
+
+tf.sort_values("tf", ascending=False)
+
+###############################
+# Barplot
+###############################
+
+tf[tf["tf"] > 500].plot.bar(x="words", y="tf")
+plt.show()
+
+###############################
+# Wordcloud
+###############################
+
+text = " ".join(i for i in df.reviewText)
+
+wordcloud = WordCloud().generate(text)
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+
+wordcloud = WordCloud(max_font_size=50,
+                      max_words=100,
+                      background_color="white").generate(text)
+plt.figure()
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+wordcloud.to_file("wordcloud.png")
+
+
+###############################
+# Şablonlara Göre Wordcloud
+###############################
+
+tr_mask = np.array(Image.open("tr.png"))
+
+wc = WordCloud(background_color="white",
+               max_words=1000,
+               mask=tr_mask,
+               contour_width=3,
+               contour_color="firebrick")
+
+wc.generate(text)
+plt.figure(figsize=[10, 10])
+plt.imshow(wc, interpolation="bilinear")
+plt.axis("off")
+plt.show()

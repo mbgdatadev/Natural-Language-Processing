aeb2881c884a39a9086e21aa20a987c70d779762
mbgdatadev <108692949+mbgdatadev@users.noreply.github.com>
2023-12-08 01:12:15 +0300

    Create sentiment-analysis.py

---
+++ Sentiment-Analysis/sentiment-analysis.py
@@ -0,0 +1,21 @@
+
+##################################################
+# 3. Sentiment Analysis
+##################################################
+
+df["reviewText"].head()
+
+# nltk.download('vader_lexicon')
+
+sia = SentimentIntensityAnalyzer()
+sia.polarity_scores("The film was awesome")
+
+
+sia.polarity_scores("I liked this music but it is not good as the other one")
+
+
+df["reviewText"][0:10].apply(lambda x: sia.polarity_scores(x))
+
+df["reviewText"][0:10].apply(lambda x: sia.polarity_scores(x)["compound"])
+
+df["polarity_score"] = df["reviewText"].apply(lambda x: sia.polarity_scores(x)["compound"])
